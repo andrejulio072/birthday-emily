a64422ca81e4c1fbc077d0d0ce63e430e5fc50b9
Andre Julio Clementino Garcia <85296705+andrejulio072@users.noreply.github.com>
2026-07-15 22:51:53 +0100

    Add scalable photo memory types

---
+++ src/photoData/types.ts
@@ -0,0 +1,9 @@
+export type PhotoMemory = {
+  id: string
+  src: string
+  alt: string
+  caption: string
+  orientation: 'portrait' | 'landscape'
+  position: string
+  chapter: 'birthday' | 'us'
+}
