8db74fd2e048255bbc69937f8205979449eefad2
Andre Julio Clementino Garcia <85296705+andrejulio072@users.noreply.github.com>
2026-07-15 23:02:06 +0100

    Activate romance and soundtrack architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppMobileOptimized from './AppMobileOptimized'
+import AppExperience from './AppExperience'
 import './styles.css'
 import './experience.css'
 import './mobile-fixes.css'
 import './performance.css'
+import './romance.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppMobileOptimized />
+    <AppExperience />
   </React.StrictMode>,
 )
